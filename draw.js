16d82bb1fcabf7c58c201d2735182b2a233b0ed1
vitia58 <30995295+vitia58@users.noreply.github.com>
2024-08-15 13:03:13 +0300

    Added gradient & increased angular

--- draw.js
+++ draw.js
@@ -15,17 +15,41 @@ const initialPositionX = Math.random() * 20 + 30; // начальное откл
 const initialPositionY = Math.random() * 20 + 30; // начальное отклонение (градусы)
 const initialVelocity = 0; // начальная скорость
 const dampingCoefficient = 0.005; // коэффициент затухания
-const angularFrequencyX = Math.random() * 0.5 + 3.5; // угловая частота
-const angularFrequencyY = Math.random() * 0.025 - 0.03 + angularFrequencyX; // угловая частота
+const angularFrequencyX = Math.random() * 10; // угловая частота
+const angularFrequencyY = Math.random() * 10; // угловая частота
 const length = 500; // длина нити маятника
 
-const timeY = Math.random() * 0.5; // время, в которое нужно узнать положение
+const timeY = 0; // время, в которое нужно узнать положение
+console.log({ initialPositionX, initialPositionY, angularFrequencyX, angularFrequencyY, timeY });
 let lastX = 0;
 let lastY = 0;
 let forNextFrame = 0;
 const svg = document.getElementById("mySVG");
+
+const updateVh = () => {
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+updateVh();
+window.onresize = updateVh;
+const color = (n) => {
+  const p = n % 765;
+  return (
+    "#" +
+    (
+      0x1000000 +
+      (Math.min(p, 255) - Math.min(Math.max(p, 255), 510) + 255) * 0x10000 +
+      (Math.min(Math.max(p, 255), 510) - Math.min(Math.max(p, 510), 765) + 255) * 0x100 +
+      (Math.max(p, 510) - Math.min(p, 255) - 255)
+    )
+      .toString(16)
+      .substring(1)
+      .toUpperCase()
+  );
+};
 (async () => {
-  for (let i = 0; i < 17500; i++) {
+  for (let i = 0; i < 20000; i++) {
     const thetaX = getPosition(
       Math.pow(i / 90.0, 1.25),
       initialPositionX,
@@ -42,21 +66,21 @@ const svg = document.getElementById("mySVG");
       angularFrequencyY,
     );
     const y = length * Math.sin(thetaY) + 450;
-    if (i != 0) {
+    if (i !== 0) {
       const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
       line.setAttribute("x1", lastX);
       line.setAttribute("y1", lastY);
       line.setAttribute("x2", x);
       line.setAttribute("y2", y);
-      line.setAttribute("stroke", "black");
+      line.setAttribute("stroke", color(i));
       line.setAttribute("stroke-width", 0.25);
       svg.appendChild(line);
     }
     lastX = x;
     lastY = y;
-    if (!(i%5)) {
-    await new Promise((res) => setTimeout(res,5));
-    //   forNextFrame = Math.pow(Math.sin((i * Math.PI) / 80000) * 1000, 0.7) + 10;
+    if (!(i % 5)) {
+      await new Promise((res) => setTimeout(res, 5));
+      //   forNextFrame = Math.pow(Math.sin((i * Math.PI) / 80000) * 1000, 0.7) + 10;
     }
   }
 })();
